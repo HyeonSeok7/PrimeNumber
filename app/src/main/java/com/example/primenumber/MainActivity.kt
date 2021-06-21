3827ba1e0841cf82ceb78aac6060c4d1f9c9c64c
startwo09 <startwo09@gamil.com>
2021-06-21 17:55:57 +0900

    [추가] 프로그래머스 소수 만들기 문제 초기 셋팅

--- app/src/main/java/com/example/primenumber/MainActivity.kt
+++ app/src/main/java/com/example/primenumber/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.primenumber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    private fun solution(nums: IntArray): Int {
+        Log.v(TAG,"numbs:$nums")
+        var answer = -1
+
+
+        return answer
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
     }
 }
